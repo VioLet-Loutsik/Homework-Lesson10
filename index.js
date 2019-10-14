5290f299f287b478b551526fee461e1c2c9e3c0b
VioLeta-Loutsik <violeta.lutic@gmail.com>
2019-10-14 22:45:34 +0300

    add new work

---
+++ index.js
@@ -0,0 +1,36 @@
+/*var helloWorld = "Hello World!"
+
+
+var today = "SAT";
+
+if (today =="SAT") {
+    console.log("Today is Saturday");
+} else {
+    console.log("working");
+
+}
+console.log(helloWorld);
+console.log(helloWorld + "My first app!");
+console.log(helloWorld + "My second app!");
+console.log(helloWorld + "My third app!");*/
+
+let mycurrentAge = "15";
+if (mycurrentAge >= "25") {
+        console.log("I am over 25 years old");
+} else {
+        console.log("I am under 25");
+}
+
+let myAge = 25;
+let result = (myAge >= 25)? "I am over 25 years old" : "I am under 25";
+console.log(result);
+
+console.log(typeof 45);
+console.log(typeof screen);
+console.log(typeof true);
+console.log(typeof book);
+console.log(typeof 'what');
+console.log(typeof decalrevalue);
+
+
+
